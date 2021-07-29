47b69e133a23722865c3a59e24716fc7f9ea1409
naoko kasahara <kassa0918@gmail.com>
2021-07-30 00:48:44 +0900

    teachers_controller, students_controller, studentにnameカラム追加

---
+++ db/migrate/20210729113700_add_colmns_to_students.rb
@@ -0,0 +1,5 @@
+class AddColmnsToStudents < ActiveRecord::Migration[6.1]
+  def change
+    add_column :students, :name, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_095845) do
+ActiveRecord::Schema.define(version: 2021_07_29_113700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_095845) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
