56f2ff75f0f4694283c0d40ca7a743a6630f5e5d
naoko kasahara <kassa0918@gmail.com>
2021-07-30 13:05:44 +0900

    teachers_tableカラム追加、日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :teachers, controllers: {registrations: 'teachers/registrations'}
   devise_for :students, controllers: {registrations: 'students/registrations'}
+  root 'homes#index' 
+  resources :homes, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
