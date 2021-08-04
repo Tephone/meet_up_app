7d55cbb291d7409b3d5d10bb395683ec862ccf92
naoko kasahara <kassa0918@gmail.com>
2021-08-04 14:30:14 +0900

    微修正

--- app/controllers/student_homes_controller.rb
+++ app/controllers/student_homes_controller.rb
@@ -5,7 +5,7 @@ class StudentHomesController < ApplicationController
       @lessons = @lessons.joins(:teacher).where('teachers.name LIKE ? ', params[:teacher] )
     end
     if  params[:language].present? 
-      @lessons = @lessons.joins(teacher: :language).where('languages.name LIKE ?', params[:language])
+      @lessons = @lessons.joins(teacher: :language).where('languages.id::text LIKE ?', params[:language])
     end
     if params[:date].present?
       @lessons = @lessons.where('start_datetime >= ? AND ? > start_datetime', params[:date].in_time_zone.beginning_of_day, params[:date].in_time_zone.end_of_day)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def languages_array
+    Language.all.map{|language| [t(language.name), language.id]}
+  end
 end
